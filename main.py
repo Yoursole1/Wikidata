a9b50f47e9bb19eb62a9ad895c6495e75b5c65db
Joshua Beard <matinzzz@icloud.com>
2026-07-23 15:54:19 -0700

    .

--- main.py
+++ main.py
@@ -18,27 +18,41 @@ def filter_wiki_links_r(wiki: str) -> list[str]:
 
     result = []
     for item in tmp:
-        s = item.split("|")[0]
-        if s[-2:] != "]]":
-            s += "]]"
-        result.append(s)
+        s: str = item.split("|")[0]
+        s = s.strip("[]")
+        s = s.strip()
+        s = f"[[{s}]]"
+
+        if ":" not in s:
+            result.append(s)
 
     return result
 
-def main():
-    root = ET.parse('small.xml').getroot()
-    for child in root:
-        article_title = child[0].text
-        article_text = ""
-        for a in child:
-            if a.tag == '{http://www.mediawiki.org/xml/export-0.11/}revision':
-                for b in a:
-                    if b.tag == '{http://www.mediawiki.org/xml/export-0.11/}text':
-                        article_text = b.text
-        if article_title == "Albedo":
-            print(article_text)
+def main() -> None:
+    with open('graph.txt', 'a') as graph_file:
+        root = ET.parse('enwiki-2026-07-01-p10p1400054.xml').getroot()
+        print("parsed")
+        i = 0
+        for child in root:
+            article_title = child[0].text
+            article_text = ""
+
+            if ":" in article_title: # not a regular article, either a user: or a talk:
+                continue
+
+            i += 1
+            if i % 1000 == 0:
+                print(article_title)
+
+            for a in child:
+                if a.tag == '{http://www.mediawiki.org/xml/export-0.11/}revision':
+                    for b in a:
+                        if b.tag == '{http://www.mediawiki.org/xml/export-0.11/}text':
+                            article_text = b.text
+
             links = filter_wiki_links_r(article_text)
-            print(f"{article_title}: {links}")
+            graph_file.write(f"{article_title}:{links}\n")
+
 
 
 
