2420173498b40438da9b1a5d1e659d4f8623831e
Joshua Beard <matinzzz@icloud.com>
2026-07-23 13:24:13 -0700

    .

--- main.py
+++ main.py
@@ -3,6 +3,28 @@ import xml.etree.ElementTree as ET
 
 
 
+def filter_wiki_links_r(wiki: str) -> list[str]:
+    links = mwparserfromhell.parse(wiki).filter_wikilinks()
+    tmp = []
+    for link in links:
+        l = filter_wiki_links_r(link[2:-2]) # take [[x]] -> x
+        if len(l) == 0:
+            tmp.append(link)
+            continue
+
+        for ele in l:
+            tmp.append(ele)
+
+
+    result = []
+    for item in tmp:
+        s = item.split("|")[0]
+        if s[-2:] != "]]":
+            s += "]]"
+        result.append(s)
+
+    return result
+
 def main():
     root = ET.parse('small.xml').getroot()
     for child in root:
@@ -13,10 +35,11 @@ def main():
                 for b in a:
                     if b.tag == '{http://www.mediawiki.org/xml/export-0.11/}text':
                         article_text = b.text
+        if article_title == "Albedo":
+            print(article_text)
+            links = filter_wiki_links_r(article_text)
+            print(f"{article_title}: {links}")
 
-        wikicode = mwparserfromhell.parse(article_text)
-        links = wikicode.filter_wikilinks()
-        print(links)
 
 
 
